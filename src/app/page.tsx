e200aad85fd362439042b2a01e5f3c6c1cb9615f
Germán Figna <german.figna@signaturit.com>
2024-08-24 11:48:07 +0200

    Separate query file. Get images only owned by user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { db } from "~/server/db";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
 
-  const images = await db.query.images.findMany({
-    orderBy: (model, { asc }) => asc(model.id)
-  });
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
